461071061dc3b2e2fed90619575d7bddbae0f0ff
Mariano Durán <marianoda20027@gmail.com>
2025-09-06 17:31:03 -0600

    New updates

--- Overtime_Project_Frontend/src/pages/Overtime/page.jsx
+++ Overtime_Project_Frontend/src/pages/Overtime/page.jsx
@@ -1,9 +1,21 @@
-// src/pages/LoginPage.js
-import OvertimeRequest from "../../components/Overtime";
+// src/pages/OvertimePage.js
+import React from "react";
+import OvertimeRequest from "../../components/Overtime"; // Asegúrate de que la ruta sea correcta
+import Header from "../../components/Header"; // Asegúrate de que la ruta sea correcta
+import Footer from "../../components/Footer"; // Asegúrate de que la ruta sea correcta
+import './styles.css'; // Estilos para la página
+
 const OvertimePage = () => {
   return (
-    <div className="Overtime-Page">
+    <div className="overtime-page">
+      {/* Header */}
+      <Header />
+      
+      {/* Componente de Overtime */}
       <OvertimeRequest />
+      
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
